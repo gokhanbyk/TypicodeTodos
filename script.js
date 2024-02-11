c6a7a49e0eab258bbf7666ce036e6c05912e394b
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-02-12 00:12:40 +0300

    getTodos func

---
+++ script.js
@@ -0,0 +1,21 @@
+const apiUrl = "https://jsonplaceholder.typicode.com/todos";
+
+const getTodos = () => {
+  fetch(apiUrl + "?_limit=10")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((todo) => {
+        const div = document.createElement("div");
+        div.appendChild(document.createTextNode(todo.title));
+        div.setAttribute("data-id", todo.id);
+
+        if (todo.completed) {
+          div.classList.add("done");
+        }
+
+        document.getElementById("todo-list").appendChild(div);
+      });
+    });
+};
+
+getTodos();
