9391227c775f66f5b96729723ba6876404827cde
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-02-12 00:13:28 +0300

    addTodoToDom func

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ const getTodos = () => {
   fetch(apiUrl + "?_limit=10")
     .then((res) => res.json())
     .then((data) => {
-      data.forEach((todo) => {
-        const div = document.createElement("div");
-        div.appendChild(document.createTextNode(todo.title));
-        div.setAttribute("data-id", todo.id);
-
-        if (todo.completed) {
-          div.classList.add("done");
-        }
-
-        document.getElementById("todo-list").appendChild(div);
-      });
+      data.forEach((todo) => addTodoToDom(todo));
     });
 };
+
+function addTodoToDom(todo) {
+  const div = document.createElement("div");
+  div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute("data-id", todo.id);
+
+  if (todo.completed) {
+    div.classList.add("done");
+  }
+
+  document.getElementById("todo-list").appendChild(div);
+}
 
 getTodos();
